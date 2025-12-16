51ce5cf4bf71dbe40ccece1230735438553dfe16
SowmisHub <vemulasowmika123@gmail.com>
2025-12-16 23:06:57 +0530

    first react page

---
+++ First React Component/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import FirstComponent from './FirstComponent'
+
+const App = () => {
+  return (
+    <div>
+      <FirstComponent/>
+      
+    </div>
+  )
+}
+
+export default App
