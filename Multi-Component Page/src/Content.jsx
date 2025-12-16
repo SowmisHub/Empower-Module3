4b4cf6ca6a5d39b548154e86c0bd3fc7595fceeb
SowmisHub <vemulasowmika123@gmail.com>
2025-12-16 23:22:37 +0530

    multi page

---
+++ Multi-Component Page/src/Content.jsx
@@ -0,0 +1,16 @@
+function Content() {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        margin: "40px",
+        fontSize: "20px",
+        padding:"15%"
+      }}
+    >
+      This is Main Content
+    </div>
+  );
+}
+
+export default Content;
---
+++ Multi-Component Page/src/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: "#f1f1f1",
+        color: "#555",
+        textAlign: "center",
+        padding: "10px",
+        fontSize: "14px"
+        
+      }}
+    >
+      This is Footer
+    </div>
+  )
+}
+
+export default Footer
