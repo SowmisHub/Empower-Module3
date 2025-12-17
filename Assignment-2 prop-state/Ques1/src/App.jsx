f20ee4b4260cfe7cc046e54598f0ae73e4d5b465
SowmisHub <vemulasowmika123@gmail.com>
2025-12-17 22:29:08 +0530

    ques1

---
+++ Assignment-2 prop-state/Ques1/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Counter from './Counter'
+
+const App = () => {
+  return (
+    <div>
+      <h2>Counter App</h2>
+      <Counter/>
+    </div>
+  )
+}
+
+export default App
